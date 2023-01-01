f9695bef3c48e4ae3ff3635943128540c6a1858b
zgscsed <1106659535@qq.com>
2023-01-01 22:43:05 +0800

    rtp包头文件

---
+++ rtp.h
@@ -0,0 +1,47 @@
+/*
+Copyright
+time: 2021.6.1
+author:zhoudong
+desc: rtpÊý¾ÝÀà£¬
+
+*/
+#ifndef RTP_H
+#define RTP_H
+
+#include <cstdint>
+
+/*
+ *
+ *    0                   1                   2                   3
+ *    7 6 5 4 3 2 1 0|7 6 5 4 3 2 1 0|7 6 5 4 3 2 1 0|7 6 5 4 3 2 1 0
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |V=2|P|X|  CC   |M|     PT      |       sequence number         |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |                           timestamp                           |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |           synchronization source (SSRC) identifier            |
+ *   +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+ *   |            contributing source (CSRC) identifiers             |
+ *   :                             ....                              :
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ */
+
+#define RTP_VESION 2
+
+#define RTP_PAYLOAD_TYPE_H264   96
+#define RTP_PAYLOAD_TYPE_AAC    97
+
+#define RTP_HEADER_SIZE         12
+#define RTP_MAX_PKT_SIZE        1400
+
+struct _RtpHeader
+{
+
+};
+
+
+
+
+#endif // !RTP_H
+
